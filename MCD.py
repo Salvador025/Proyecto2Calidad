bf4179f15e33af50ba348eee8205ee564c47b7af
Salvador025 <jose.mariscal@iteso.mx>
2023-10-15 15:26:45 -0600

    Codigo terminado MCD y NumeroMáximo

--- MCD.py
+++ MCD.py
@@ -1,5 +1,18 @@
-def main():
-    x = int(input("Introduce x: "))
-    y = int(input("Introduce y: "))
-
-    if x
+def mcd(x, y):
+    if x <= 0 or y <= 0:
+        print("Error: Los números deben ser positivos")  # Error
+        return -1
+    elif x == 1 or y == 1:
+        return 1
+    else:
+        while x != y:
+            if x > y:
+                x = x - y
+            else:
+                y = y - x
+        return x
+    
+if __name__ == "__main__":
+    X = int(input("Ingrese el primer número: "))
+    Y = int(input("Ingrese el segundo número: "))
+    print("El MCD es: ", mcd(X, Y))
